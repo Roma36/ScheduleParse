f176c9b640ef67b3b7e13673c008d9e06caea617
Roma36 <romagrin36@gmail.com>
2014-12-14 20:43:24 +0300

    Schedule
    One day schedule ready

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 function getXml(groupNumber) {
 
     var scheduleLocalPath = "schedules/" + groupNumber + ".xml";
-    var scheduleWebPath = 'https://cors-anywhere.herokuapp.com/'+'http://www.bsuir.by/schedule/rest/schedule/' + groupNumber;
+    var scheduleWebPath = 'https://cors-anywhere.herokuapp.com/' + 'http://www.bsuir.by/schedule/rest/schedule/' + groupNumber;
     $.ajax({
         type: "GET",
         url: scheduleLocalPath,
@@ -22,27 +22,62 @@ function getXml(groupNumber) {
 
 }
 
+function syncTemplateData(lesson) {
+    var lessonItems = {};
 
+    var weekNumbers = "";
+    if (lesson.weekNumber.length === undefined) {
+        weekNumbers += lesson.weekNumber["#text"];
+    } else {
+        $.each(lesson.weekNumber, function(i, week) {
+            if (week["#text"] === "0") {
+                weekNumbers = "";
+                return false;
+            }
+            weekNumbers += week["#text"] + " ";
+        });
+    }
+    lessonItems.weeks = weekNumbers;
+    lessonItems.subject = lesson.subject["#text"];
+    lessonItems.lessonType = lesson.lessonType["#text"];
+    if (lesson.numSubgroup["#text"] !== "0") {
+        lessonItems.numSubGroup = lesson.numSubgroup["#text"];
+    } else {
+        lessonItems.numSubGroup = "";
+    }
+
+    if (lesson.auditory !== undefined) {
+        lessonItems.room = lesson.auditory["#text"];
+    }
+    lessonItems.time = lesson.lessonTime["#text"];
+    if (lesson.employee !== undefined) {
+        lessonItems.teacher = lesson.employee.firstName["#text"] + " " + lesson.employee.middleName["#text"] + " " + lesson.employee.lastName["#text"];
+    }
+
+    $('#lessonTmpl').tmpl(lessonItems).appendTo('.daily-schedule');
+}
 
 function parseXml(xml) {
-    jsonSchedule = xmlToJson(xml);
+    var jsonSchedule = xmlToJson(xml).scheduleXmlModels.scheduleModel;
 
-    alert(jsonSchedule);
+    var day = 1;
+
+    var count = 0;
+
+
+
+    $('.daily-schedule').empty();
+
+    if (jsonSchedule[day].schedule.length === undefined) {
+        syncTemplateData(jsonSchedule[day].schedule);
+
+    } else {
+        $.each(jsonSchedule[day].schedule, function(i, lesson) {
+            syncTemplateData(lesson)
+        });
+
+    }
+
+
+
 }
-
-
-
-function loadXMLDoc(path) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", path, true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            var xmlSchedule = xhr.responseXML;
-            parseXml(xmlSchedule);
-        }
-    };
-    xhr.send(null);
-}
-
-
-
